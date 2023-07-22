003c9b048f24e81c9be38de5c3dea912b1c325eb
muahmed <muahmed@ptc.com>
2023-07-22 15:29:14 -0700

    Initial Setup بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ

---
+++ src/main/java/com/acme/ai/hai/HaiApp.java
@@ -0,0 +1,13 @@
+package com.acme.ai.hai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaiApp.class, args);
+	}
+
+}
